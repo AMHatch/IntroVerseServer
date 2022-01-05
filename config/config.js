0582a915ce091606b3ff6e532db32b3f5e4ace8d
Andrew Hatch <89485988+AMHatch@users.noreply.github.com>
2022-01-05 14:11:09 -0500

    added test to config

--- config/config.js
+++ config/config.js
@@ -7,5 +7,13 @@ module.exports = {
     "database": process.env.DB_DATABASE,
     "host": process.env.DB_HOST,
     "dialect": "postgres"
+},
+"test": {
+    "use_env_variable": process.env.DATABASE_URL,
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres"
 }
-}
+}   
